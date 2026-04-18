1f5a7da8cb611f58c9fd51197134df63444fbf8d
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-04-18 12:51:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,11 @@
-const CACHE_NAME = 'ace-manager-static-v0-1-0';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './admin.html',
-  './css/main.css',
-  './js/main.js',
-  './js/admin.js',
-  './build/build-info.json',
-  './content/manifest.json',
-  './assets/icons/favicon.svg',
-  './assets/icons/logo-mark.svg',
-  './assets/icons/icon-192.png',
-  './assets/icons/icon-512.png',
-  './assets/backgrounds/bg-dashboard.svg',
-  './assets/backgrounds/bg-court-night.svg'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.clients.claim();
+    const clients = await self.clients.matchAll({ type: 'window' });
+    clients.forEach(client => client.navigate(client.url));
+  })());
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-      const clone = response.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-      return response;
-    }).catch(() => caches.match('./index.html')))
-  );
-});
+self.addEventListener('fetch', () => {});
